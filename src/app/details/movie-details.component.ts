c238aafd97a1c60f379a562147337221c2547144
BarrieLAJ <41100890+BarrieLAJ@users.noreply.github.com>
2019-11-01 03:02:19 -1200

    Added Details Feature

--- src/app/details/movie-details.component.ts
+++ src/app/details/movie-details.component.ts
@@ -3,12 +3,37 @@ import { MoviesService } from '../services/movies.service'
 import { ActivatedRoute } from '@angular/router'
 
 @Component({
-    selector: 'movie-details',
-    template: `
-        <div class="row">
-            <h1>{{movie?.title}}</h1>
-        </div>
-    `
+    templateUrl: './movie-details.component.html',
+    styles: [`
+        .text-white{
+            color: #fff !important;
+            font-size: 1.0em !important;
+        }
+        .movie-title{
+            font-size: 2.0em;
+            letter-spacing: 3px;
+            font-weight: bolder;
+            margin-buttom: 5px;
+        }
+        movie-overview{
+            font-size: 0.02em;
+            font-weight: 20px;
+            text-align: justify;
+            max-height: 150px;
+            max-width: 300px;
+            overflow: hidden;
+        }
+        .text-white>div>span{
+            float: right;
+        }
+        .list-genre>li{ 
+            margin: 20px;
+        }
+        .row > h4{
+            margin-top: 10px;
+        }
+
+    `]
 })
 
 export class MovieDetailsComponent implements OnInit {
--- src/app/details/tvshow-details.component.ts
+++ src/app/details/tvshow-details.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { TvShowsService } from '../services/tvshows.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-    selector: 'tvshow-details',
-    template: `
-        <div class="row">
-            <h1>Tv Show Title</h1>
-        </div>
-    `
+    templateUrl: 'tvshow-details.component.html',
+    styles: [`
+        .text-white{
+            color: #fff !important;
+            font-size: 1.0em !important;
+        }
+        .movie-title{
+            font-size: 2.0em;
+            letter-spacing: 3px;
+            font-weight: bolder;
+            margin-buttom: 5px;
+        }
+        movie-overview{
+            font-size: 0.02em;
+            font-weight: 20px;
+            text-align: justify;
+            max-height: 150px;
+            max-width: 300px;
+            overflow: hidden;
+        }
+        .text-white>div>span{
+            float: right;
+        }
+        .list-genre>li{ 
+            margin: 20px;
+        }
+        .row > h4{
+            margin-top: 10px;
+        }
+    `]
 })
 
-export class TvShowDetailsComponent {
-    
+export class TvShowDetailsComponent implements OnInit {
+    tvshow:any
+    constructor(private tvshowService:TvShowsService, private router:ActivatedRoute){
+
+    }
+    ngOnInit(){
+        this.tvshow = this.tvshowService.getTvshow(+this.router.snapshot.params['id'])
+        console.log(this.tvshow)
+    }
 }
